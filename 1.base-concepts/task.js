0a35cff16eb68e7df102f04037082efcb2bbde12
Irina Nazarova <irignazarova@gmail.com>
2024-07-02 21:09:31 +0300

    1 задача без ошибок

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,11 +4,12 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d;
   d=b**2-4*a*c;
-  if (d===0){
-    arr.push(-b/(2*a));
-  } else if (d>0) {
-      [arr.push((-b + Math.sqrt(d) )/(2*a)),arr.push((-b - Math.sqrt(d)/(2*a)))]
-    }
+  
+  if (d>0){
+    arr=[((-b + Math.sqrt(d) )/(2*a)),((-b - Math.sqrt(d) )/(2*a))]
+  } else if (d===0){
+    arr = [(-b/(2*a))]
+  }
   
   return arr;
 }
