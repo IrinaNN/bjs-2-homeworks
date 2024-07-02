8a132f5b104505f893be3a04bd5e27ab3fe812a3
Irina Nazarova <irignazarova@gmail.com>
2024-07-02 20:43:40 +0300

    исправление 1 задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,8 +7,7 @@ function solveEquation(a, b, c) {
   if (d===0){
     arr.push(-b/(2*a));
   } else if (d>0) {
-      arr.push((-b + Math.sqrt(d) )/(2*a));
-      arr.push((-b - Math.sqrt(d)/(2*a)))
+      [arr.push((-b + Math.sqrt(d) )/(2*a)),arr.push((-b - Math.sqrt(d)/(2*a)))]
     }
   
   return arr;
