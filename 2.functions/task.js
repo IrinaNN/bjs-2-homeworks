397b80c40319c56d43f5b5c0f0e8629ccc40db49
Irina Nazarova <irignazarova@gmail.com>
2024-07-25 23:10:01 +0300

    1задача

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,24 @@
-function getArrayParams(...arr) {
+'use strict'
+// задача 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i=0;i<arr.lenght;i++){
+    if (arr[i]<min){
+      min=arr[i]
+    } if (arr[i]>max){
+      max=arr[i]
+    }
+    sum=sum+arr[i]
+  }
+  avg=Number((sum/arr.lenght).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
+// задача 2
 function summElementsWorker(...arr) {
 
 }
@@ -18,7 +34,7 @@ function differenceEvenOddWorker(...arr) {
 function averageEvenElementsWorker(...arr) {
 
 }
-
+// задача 3
 function makeWork (arrOfArr, func) {
 
 }
