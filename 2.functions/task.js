257cb70e7d089c9eccf7293de84e48b5700bf0df
Irina Nazarova <irignazarova@gmail.com>
2024-07-25 23:42:36 +0300

    задача 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,22 +1,21 @@
 'use strict'
 // задача 1
 function getArrayParams(arr) {
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-  let avg;
-  let step=0;
-  for (let i=0;i<arr.lenght;i++){
-    if (arr[i]<min){
-      min=arr[i]
-    } else 
-    if (arr[i]>max){
-      max=arr[i]
+  let min,max,sum,avg;
+  min=arr[0];
+  max=arr[0];
+  sum=0;
+
+  for (let i=0;i<arr.length;i++){
+   if (arr[i]<min){
+     min=arr[i];
+    } else if (arr[i]>max){
+      max=arr[i];
     }
-    sum=sum+arr[i]
-    step=step+1;
+    sum=sum+arr[i];
+    
   }
-  avg = Number((sum/step).toFixed(2));
+  avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
