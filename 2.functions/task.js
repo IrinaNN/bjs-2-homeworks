f046b294a08af394b175319d94534b2474f17d86
Irina Nazarova <irignazarova@gmail.com>
2024-07-25 23:19:58 +0300

    1 задача

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,16 +5,18 @@ function getArrayParams(arr) {
   let max = -Infinity;
   let sum = 0;
   let avg = 0;
+  let step=0;
   for (let i=0;i<arr.lenght;i++){
-    sum=sum+arr[i];
+    
     if (arr[i]<min){
       min=arr[i]
     } if (arr[i]>max){
       max=arr[i]
     }
     sum=sum+arr[i]
+    step=arr.lenght
   }
-  avg=Number((sum/arr.lenght).toFixed(2));
+  avg=Number((sum/step).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
